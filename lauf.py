152abae06f5a79f3f0f3050fd64c47302649241f
hansen1416 <badapplesweetie@gmail.com>
2024-03-23 17:46:34 +0800

    1

--- lauf.py
+++ lauf.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import subprocess
+
 from oss2_uploader import folder_downloader, folder_uploader
 
 videos_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "videos")
@@ -7,8 +9,37 @@ videos_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "videos")
 # iterat over all files in the videos directory, use glob.iglob()
 videos_list = glob.iglob(videos_dir + "/*.avi")
 
-for video in videos_list:
-    print(video)
+for video_path in videos_list:
+    video_name = os.path.basename(video_path)
+
+    # use subprocess to run the command
+    subprocess.run(
+        [
+            "python3",
+            "evaludate.py",
+            "-d",
+            "20240322-2086",
+            "-arc",
+            "3,3,3,3,3",
+            "-c",
+            "checkpoint",
+            "--evaluate",
+            "pretrained_h36m_detectron_coco.bin",
+            "--render",
+            "--viz-subject",
+            video_name,
+            "--viz-action",
+            "custom",
+            "--viz-camera",
+            "0",
+            "--viz-video",
+            video_path,
+            "--viz-export",
+            video_name,
+            "--viz-size",
+            "6",
+        ]
+    )
 
 
 """
